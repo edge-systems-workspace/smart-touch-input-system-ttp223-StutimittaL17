5b8b62fd6ec780d4450f6d5c826b171d2c93f312
StutimittaL17 <stuti.mittal_cs.aiml24@gla.ac.in>
2026-02-18 15:12:56 +0530

    Define touch and LED pin constants
    Added pin definitions for touch sensor and LED.

--- src/main.cpp
+++ src/main.cpp
@@ -10,4 +10,16 @@
  * @author Stutimittal_17
  * @date 18 Feb 2026
  */
+// Pin Definitions
+int touchPin = 7;   ///< Touch sensor connected to digital pin 7
+int ledPin = 13;    ///< LED connected to digital pin 13
 
+
+/**
+ * @brief Initializes serial communication and pin modes.
+ *
+ * This function runs once at startup.
+ * - Starts Serial communication at 9600 baud rate
+ * - Sets touchPin as INPUT
+ * - Sets ledPin as OUTPUT
+ */
